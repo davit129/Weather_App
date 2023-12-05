d3c55b9e66339f0635997375560742149924c9a7
davit129 <125029267+davit129@users.noreply.github.com>
2023-12-05 10:45:25 +0400

    Add files via upload

---
+++ src/components/AnimationFront.vue
@@ -0,0 +1,45 @@
+<template>
+  <div ref="lottieContainer" class="lottie-container"></div>
+</template>
+
+<script>
+9;
+import lottie from "lottie-web";
+export default {
+  props: {
+    Path: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      animation: null,
+    };
+  },
+  watch: {
+    Path: {
+      immediate: true,
+      handler(value) {
+        if (this.animation) {
+          lottie.destroy();
+        }
+
+        this.animation = lottie.loadAnimation({
+          container: this.$refs.lottieContainer,
+          renderer: "svg",
+          loop: true,
+          autoplay: true,
+          path: value,
+        });
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.lottie-container {
+  zoom: 50%;
+}
+</style>
